7d3c7d7d0796fc473ecf9fa8de32f30d98d82726
zhouxingrong <1085257460@qq.com>
2020-05-14 13:40:56 +0800

    结构修改

--- ds-platform.sh
+++ ds-platform.sh
@@ -7,29 +7,29 @@
 !
 #-----------------------------------------------------
 #是否发送通知邮件
-sendEmail=true
+sendEmail=false
 #接收邮箱
 readonly email_no=user@163.com
 #邮件标题
-email_title='澧县单采血浆站maxwell服务通知'
+email_title='XXX maxwell服务通知'
 
-#定时监听进程,停顿时间（秒）,即每多少时间进行一次服务进程监听,默认5分钟
+#定时监听进程,停顿时间（秒）,即每多少时间进行一次服务进程监听
 readonly delay_millisecond=300
 
 #宕机时，最大重启次数
-total_count=2016
+total_count=20200
 
 #应用名ID,注意：此sh脚本全路径名不能包含此应用名,覆盖config.properties里的client_id
 export readonly APP_NAME="maxwell"
 
 #maxwell运行日志删除，默认删除
-delete_log=true
+delete_log=false
 
 #邮件附件最大值,默认10M
 email_accessory_maxsize=$((1024 * 1024 * 10))
 
 #截取问题日志前多少行
-analysisLog_pre=20
+analysisLog_pre=50
 #截取问题日志后多少行
 analysisLog_later=100
 
@@ -42,7 +42,7 @@ export readonly CONSOLE_OUTPUT_FILE=$CURDIR/logs/$APP_NAME.out
 readonly APP_DIR="maxwell-1.24.1/bin/maxwell"
 
 #日志分析关键字符串
-readonly analysisLog_str='Exception:'
+readonly analysisLog_str='Exception:,ERROR:'
 
 #邮件进程名
 readonly email_app=sendmail
@@ -56,27 +56,38 @@ analysisLog(){
 
     #存在日志
     if test -e $CONSOLE_OUTPUT_FILE; then
+
         #获取异常所在行
-        local cur_line=`grep -n "${analysisLog_str}" $CONSOLE_OUTPUT_FILE | tail -n 1`
-        if [ "$cur_line" != "" ]; then
-            #行数
-            cur_line=${cur_line%%:*}
-            local line_pre=$((analysisLog_pre > cur_line ? 1 : `expr $cur_line - $analysisLog_pre`))
-            local line_later=`expr $cur_line + $analysisLog_later`
-            #获取指定长度的日志信息
-            
-            error=`sed -n "${line_pre},${line_later}p" $CONSOLE_OUTPUT_FILE`
-        fi
+        local array=(${analysisLog_str//,/ })
+        local cur_line=''
+        for var in ${array[@]}
+        do
+           cur_line=`grep -n "${var}" $CONSOLE_OUTPUT_FILE | tail -n 1`
+           if [ "$cur_line" != "" ]; then
+                #行数
+                cur_line=${cur_line%%:*}
+                local line_pre=$((analysisLog_pre > cur_line ? 1 : `expr $cur_line - $analysisLog_pre`))
+                local line_later=`expr $cur_line + $analysisLog_later`
+                #获取指定长度的日志信息
+                error=`sed -n "${line_pre},${line_later}p" $CONSOLE_OUTPUT_FILE`
+                break
+            fi
+        done
     fi
     echo "${error}"
 }
 
 #邮件发送
 send(){
+    #不允许发送邮件
+    if [ $sendEmail != true ]; then
+        return 1
+    fi
+
     #存在sendMail 进程才进行邮件发送
     local mail_process=`ps aux | grep ${email_app} | grep -v grep`;
 
-    echo "sendMail process: $mail_process "
+    logger "sendMail process: $mail_process "
     if [ "$mail_process" != "" ]; then
         local title=$1
         #错误日志
@@ -85,12 +96,12 @@ send(){
         local accessory=$3
         local count=1
         #发送邮件
-        
+
         #判断附件是否存在
         if test -e $accessory ;then
             #获取日志文件大小
             local filesize=`ls -l $accessory | awk '{ print $5 }'`
-            echo "当前待发送邮件附件大小为:$filesize "
+            logger "当前待发送邮件附件大小为:$filesize "
             if [ $filesize -gt $email_accessory_maxsize ]; then
                 accessory=''
             fi
@@ -107,7 +118,7 @@ send(){
             else
                 echo -e "${content}"|mail -s ${title} -a $accessory $email_no
             fi
-            
+
             if [ $? = 0 ]; then
                 return $?
             fi
@@ -116,10 +127,10 @@ send(){
             sleep 5
         done
 
-        echo "邮件发送重试${email_try_count}次失败..."
+        logger "邮件发送重试${email_try_count}次失败..."
         return 1
     else
-        echo "${email_app}邮箱客户端未启动,请确认..."
+        logger "${email_app}邮箱客户端未启动,请确认..."
         return 1
     fi
 }
@@ -133,10 +144,10 @@ kill_app() {
     do
         local process=`ps -ef | grep ${processName} | grep -v grep |grep -v "\<${notExistentPid}\>" | awk '{print $2}'`
         if [ "$process" = "" ]; then
-            echo "no $processName process";
+            logger "no $processName process";
             break;
         else
-            echo -e "kill $processName process...";
+            logger "kill $processName process...";
             kill -9 $process
             sleep 5
         fi
@@ -155,17 +166,17 @@ stop(){
 
 #启动
 run_app(){
-    echo "start maxwell process...";
+    logger "start maxwell process...";
     #删除旧日志
     if [ $delete_log = true -a -e $CONSOLE_OUTPUT_FILE ]; then
-        echo "删除旧日志$CONSOLE_OUTPUT_FILE"
-        rm -rf $CONSOLE_OUTPUT_FILE
+        logger "删除旧日志$CONSOLE_OUTPUT_FILE"
+        rm -rf "$CURDIR/logs/$APP_NAME.out"
     fi
     #启动应用
     ${CURDIR}/${APP_DIR} \
         --config=${CURDIR}/config.properties \
         --client_id=$APP_NAME \
-        --daemon 
+        --daemon
 }
 
 start(){
@@ -185,29 +196,27 @@ start(){
     while (($start_count <= $total_count))
     do
         local process=`ps aux | grep ${APP_NAME} | grep -v grep`;
-        echo "maxwell process:$process"
+        logger "maxwell process:$process"
         #未启动
         if [ "$process" = "" ]; then
-            echo "no maxwell process";
-            
-            #echo "是否发送邮件:${sendEmail},是否可以发送邮件:${isSend}"
+            logger "no maxwell process";
+
             #发送邮件
-            if [ $sendEmail = true -a $isSend = false ]; then
+            if [ $isSend = false ]; then
                 local error=$(analysisLog)
-                #echo "错误日志：${error},是否发送附件:${exist_accessory},附件:$CONSOLE_OUTPUT_FILE"
                 #发送邮件
                 send "警告：${email_title}" "maxwell服务已宕机，正在尝试重启，错误日志为：\n $error" $CONSOLE_OUTPUT_FILE
                 if [ $? = 0 ]; then
                     isSend=true
-                    echo 'maxwell宕机邮件已投递成功...'
+                    logger 'maxwell宕机邮件已投递成功...'
                 else
-                    echo 'maxwell宕机邮件投递失败...'
+                    logger 'maxwell宕机邮件投递失败...'
                 fi
             fi
             #启动
             run_app
 
-            echo "maxwell故障重启次数：${start_count}"
+            logger "maxwell故障重启次数：${start_count}"
 
             #启动次数累加
             ((start_count++))
@@ -229,7 +238,7 @@ start(){
                 send "通知：${email_title}" "maxwell 已启动成功..." $CONSOLE_OUTPUT_FILE
             fi
         fi
-        
+
         sleep $delay_millisecond
     done
 }
@@ -238,32 +247,38 @@ restart() {
     start;
 }
 
+logger(){
+    local content=$1
+    local date=`date '+%Y-%m-%d %H:%M:%S'`
+    printf "%s\n" "$date $content"
+}
+
 #校验
 #1、脚本名必须与上一级目录一致
 #2、sh脚本绝对路径不能包含AppName名称
 verify(){
-    echo "执行校验中..."
+    logger "执行校验中..."
 
     local parent_dir=${CURDIR##*/}
-    echo "脚本父目录：$parent_dir"
+    logger "脚本父目录：$parent_dir"
 
     local filePath=$0
     local filename=${filePath##*/}
 
     filePath="$CURDIR/$filename"
-    echo "脚本文件文件名：$filename"
-    echo "全路径名：$filePath"
+    logger "脚本文件文件名：$filename"
+    logger "全路径名：$filePath"
 
-    echo "应用名ID：$APP_NAME"
+    logger "应用名ID：$APP_NAME"
 
     local result=$(echo $filePath | grep "${APP_NAME}")
     if [ "$result" != "" ]; then
-        echo "脚本文件全路径名不能包含应用名，请进行修改..."
+        logger "脚本文件全路径名不能包含应用名，请进行修改..."
         exit 1
     fi
 
     if [ "$parent_dir.sh" != "$filename" ]; then
-        echo "脚本父目录必须跟sh脚本名一致，请进行修改..."
+        logger "脚本父目录必须跟sh脚本名一致，请进行修改..."
         exit 1
     fi
 }
@@ -283,7 +298,7 @@ case "$1" in
         restart
         ;;
     *)
-    echo "usage: $0 {start|stop|restart}"
+    logger "usage: $0 {start|stop|restart}"
     exit 1
         ;;
     esac
